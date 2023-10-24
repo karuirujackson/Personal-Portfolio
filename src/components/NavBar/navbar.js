a34026992f5b71325e9750d94bfe8cbe4818eceb
karuirujackson <jkjackaruiru641@gmail.com>
2023-10-24 10:18:38 +0300

    Personal Portfolio components update

---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.css'
+ 
+const  Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <img src='' alt='' />
+            div.desktopMenu
+
+        </nav>
+
+         
+    );
+};
+ 
+export default Navbar;
+
+
